c93ea6ea03d8e9336bef413fff3387b89b0ecf88
Jose Valles <jose.valles.leon@gmail.com>
2024-06-10 21:43:41 -0300

    update waterbalance and station status product for 2024-05

---
+++ HydroSOS/run_csv_to_json_stations.bat
@@ -0,0 +1,7 @@
+pause
+"c:\Users\DINAGUA\anaconda3\envs\HydroSOS\python.exe" "d:\GitHub\GT-HRH\HydroSOS\csv_to_json.py" output_csv/01_month output_json/01_month
+timeout 5
+"c:\Users\DINAGUA\anaconda3\envs\HydroSOS\python.exe" "d:\GitHub\GT-HRH\HydroSOS\csv_to_json.py" output_csv/03_month output_json/03_month
+timeout 5
+"c:\Users\DINAGUA\anaconda3\envs\HydroSOS\python.exe" "d:\GitHub\GT-HRH\HydroSOS\csv_to_json.py" output_csv/12_month output_json/12_month
+pause
