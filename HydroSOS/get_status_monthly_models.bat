c04b79ba90b6d25c2829b75279f5b2ec14fc4fbd
Jose Valles <jose.valles.leon@gmail.com>
2024-08-27 20:04:03 -0300

    update files and housecleaning

--- HydroSOS/get_status_monthly_models.bat
+++ HydroSOS/get_status_monthly_models.bat
@@ -1,3 +1,5 @@
 pause
+c:/Users/DINAGUA/anaconda3/envs/HydroSOS/python.exe python_scripts/monthly_status.py "monthly" "./waterbalance/input/" "./waterbalance/output_csv/01_month/" "./waterbalance/output_json/01_month/"
 c:/Users/DINAGUA/anaconda3/envs/HydroSOS/python.exe python_scripts/quaterly_status.py "monthly" "./waterbalance/input/" "./waterbalance/output_csv/03_month/" "./waterbalance/output_json/03_month/"
+c:/Users/DINAGUA/anaconda3/envs/HydroSOS/python.exe python_scripts/annualy_status.py "monthly" "./waterbalance/input/" "./waterbalance/output_csv/12_month/" "./waterbalance/output_json/12_month/"
 pause
