3c043d261b15e9d0dac58f0835740096ab7ee812
Jose Valles <jose.valles.leon@gmail.com>
2024-09-04 16:47:11 -0300

    update status results

--- HydroSOS/get_status_monthly_stations.bat
+++ HydroSOS/get_status_monthly_stations.bat
@@ -1,3 +1,3 @@
 pause
-c:\Users\DINAGUA\anaconda3\envs\HydroSOS\python.exe python_scripts/calculate_hydrological_status.py "daily" "./data/" "./output_csv/01_month/" "./output_json/01_month/"
+c:\Users\DINAGUA\anaconda3\envs\HydroSOS\python.exe python_scripts/monthly_status.py "daily" "./data/" "./output_csv/01_month/" "./output_json/01_month/"
 pause
